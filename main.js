a01b930296ce1596c64b9a65213f2ad6fde546b9
Itamar Yekutiel <yekutiel.itamar@gmail.com>
2024-11-08 10:07:32 +0200

    did frist coomit to webpage

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
-console.log("Hello World!");
-setupCounter();
+//console.log("Hello World!");
+//setupCounter();
 
-function setupCounter() {
-  let count = 0;
+// Array of image paths
+const images = [
+  "https://raw.githubusercontent.com/dohliam/html5-sample-media/master/Coloured%2C_textured_craft_card_edit.jpg",
+  "https://raw.githubusercontent.com/dohliam/html5-sample-media/master/Opening_chess_position_from_black_side.jpg",
+  "https://raw.githubusercontent.com/dohliam/html5-sample-media/master/Colouring_pencils.jpg"
+  // Add more images as needed
+];
 
-  function increment() {
-    count++;
-    document.querySelector("#count").innerHTML = count;
-  }
+let currentIndex = 0;
 
-  function decrement() {
-    count--;
-    document.querySelector("#count").innerHTML = count;
-  }
+const imgElement = document.getElementById("counter-image");
+const changePictureButton = document.getElementById("change-picture");
 
-  document.querySelector("#increment").addEventListener("click", increment);
-  document.querySelector("#decrement").addEventListener("click", decrement);
+// Function to update the image
+function updateImage() {
+  imgElement.src = images[currentIndex];
 }
+
+// Event listener to change the picture on button click
+changePictureButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateImage();
+});
+
+window.addEventListener("load", () => {
+  const introText = document.getElementById("intro-text");
+  introText.classList.add("slide-in");
+});
+
+const counterImage = document.getElementById("counter-image");
+
+counterImage.addEventListener("click", () => {
+  counterImage.classList.toggle("rotate");
+});
+
+
+// function setupCounter() {
+//   let count = 0;
+
+//   function increment() {
+//     count++;
+//     document.querySelector("#count").innerHTML = count;
+//   }
+
+//   function decrement() {
+//     count--;
+//     document.querySelector("#count").innerHTML = count;
+//   }
+
+//   document.querySelector("#increment").addEventListener("click", increment);
+//   document.querySelector("#decrement").addEventListener("click", decrement);
+// }
